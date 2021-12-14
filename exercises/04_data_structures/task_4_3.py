cad9f4c4e8fce775980efd046a217acac4300a47
ZaynulliMA <zaynullinma@yandex.ru>
2021-12-15 04:29:35 +0500

    5

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -21,4 +21,10 @@
 проверять результат.
 """
 
+from types import resolve_bases
+
+
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+a=config.split()
+result=a[-1].split(',')
+print(result)
