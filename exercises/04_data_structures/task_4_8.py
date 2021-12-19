6abb9f1ea7a87d44754347fac013ab80ab84a585
ZaynullinMA <zaynullinma@yandex.ru>
2021-12-19 06:15:11 +0500

    fix 4.8

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -32,5 +32,5 @@ oct2 = int(oct2)
 oct3 = int(oct3)
 oct4 = int(oct4)
 print(f'''
-{oct1:<8} {oct2:<8} {oct3:<8} {oct4:<8}
+{oct1:<10}{oct2:<10}{oct3:<10}{oct4:<10}
 {oct1:08b}  {oct2:08b}  {oct3:08b}  {oct4:08b}''')
