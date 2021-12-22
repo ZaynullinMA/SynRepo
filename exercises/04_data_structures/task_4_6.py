2bf800b6a2f96ac8d0cf290459062868c565a77d
ZaynullinMA <zaynullinma@yandex.ru>
2021-12-23 01:23:45 +0500

    fix 4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -18,10 +18,10 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
-a,b,c,d,e,f = ospf_route.split()
+ospf_route = ospf_route.split()
 print(f'''
-Prefix                {a}
-AD/Metric             {b.strip('[]')}
-Next-Hop              {d.rstrip(',')}
-Last update           {e.rstrip(',')}
-Outbound Interface    {f}''')
+Prefix                {ospf_route[0]}
+AD/Metric             {ospf_route[1].strip('[]')}
+Next-Hop              {ospf_route[3].rstrip(',')}
+Last update           {ospf_route[4].rstrip(',')}
+Outbound Interface    {ospf_route[5]}''')
