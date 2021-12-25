2fa528b0e0a17bcf7bb7d54e9120fa52716a80a4
ZaynullinMA <zaynullinma@yandex.ru>
2021-12-25 17:51:52 +0500

    5.3, 5.3a done

--- exercises/05_basic_scripts/task_5_3.py
+++ exercises/05_basic_scripts/task_5_3.py
@@ -63,3 +63,23 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+from sys import argv
+
+access_template = '\n'.join(access_template)
+trunk_template = '\n'.join(trunk_template)
+
+templates = {
+    "access": access_template,
+    "trunk": trunk_template
+}
+
+input1 = input('Введите режим работы интерфейса (access/trunk):')
+input2 = input('Введите тип и номер интерфейса:')
+input3 = input('Введите номер влан(ов):')
+
+output = """
+interface {0:}
+{1:}
+"""
+print(output.format(input2, templates[input1]).format(input3))
--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -25,3 +25,27 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+from sys import argv
+
+access_template = '\n'.join(access_template)
+trunk_template = '\n'.join(trunk_template)
+
+templates = {
+    "access": access_template,
+    "trunk": trunk_template
+}
+templates1 = {
+    "access": 'Введите номер VLAN:',
+    "trunk": 'Введите разрешенные VLANы:'
+}
+
+input1 = input('Введите режим работы интерфейса (access/trunk):')
+input2 = input('Введите тип и номер интерфейса:')
+input3 = input(templates1[input1])
+
+output = """
+interface {0:}
+{1:}
+"""
+print(output.format(input2, templates[input1]).format(input3))
