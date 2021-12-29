25822e41c290f78ca86251568ca13fa0e425175c
ZaynullinMA <zaynullinma@yandex.ru>
2021-12-29 19:43:43 +0500

    6.1, 6.2 done, 6.2a start

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,28 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите ip адрес в формате 10.0.1.1: ')
+ip_correct = False
+
+while not ip_correct:
+   if ''.join(ip.split('.')).isdigit() == False:
+      print('Неправильный IP-адрес')
+      break
+   elif len(ip.split('.')) != 4:
+      print('Неправильный IP-адрес')
+      break
+   elif int(ip.split('.')[0]) >=1 and int(ip.split('.')[0])<=223:
+      print('unicast')
+      ip_correct = True
+   elif int(ip.split('.')[0]) >=224 and int(ip.split('.')[0])<=239:
+      print('multicast')
+      ip_correct = True
+   elif ip == '255.255.255.255':
+      print('local broadcast')
+      ip_correct = True
+   elif ip == "0.0.0.0":
+      print('unassigned')
+      ip_correct = True
+   else:
+      print('unused')
+      ip_correct = True
